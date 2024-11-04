1997e6038667db0e41141659c78eb9111ff93bfd
Devsamim <dev.samim.kolkata@gmail.com>
2024-11-04 12:11:37 +0530

    header section complete

---
+++ src/main.jsx
@@ -0,0 +1,25 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import Root from "./components/Root/Root";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+
+    children: [
+      {
+        path: "/",
+        element: <h2></h2>,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
